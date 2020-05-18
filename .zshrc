bdb360aad15fd4dc8e726fdd8c6297003defdf85
czystyl <lukasz.czyszczonik@gmail.com>
2020-05-18 18:34:32 +0200

    Update setup

--- .zshrc
+++ .zshrc
@@ -1,17 +1,1 @@
-# Source Prezto.
-if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
-  source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
-fi
-
-zstyle ':prezto:module:autosuggestions' color 'yes'
-
-# NVM CONFIG
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-export EDITOR=nvim
-source $HOME/.zsh-background-notify/bgnotify.plugin.zsh
-
-eval "$(rbenv init -)"
-eval $(opam config env)
+/Users/czysty/.zprezto/runcoms/zshrc
--- setup_prezto.sh
+++ setup_prezto.sh
@@ -1,23 +1,9 @@
 #!/bin/zsh
 
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+ 
 setopt EXTENDED_GLOB
 
-DIR="${ZDOTDIR:-$HOME}"/.zprezto
-
-if test -d "$DIR"; then
-    cd $DIR
-    
-    git pull
-    git submodule update --init --recursive
-else
-  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-
-  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-    # do not sync .zshrc 
-    if [ $rcfile:t != "zshrc" ]; then
-      # use --force
-      ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"      
-    fi
-  done
-fi
-
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
